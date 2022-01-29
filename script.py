c1214661a889fb2b26eeff75cd9d31e6f49dde08
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-01-29 21:33:46 +0000

    Add songs to the Acoustic Guitar Dictionary

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-acoustic_guitar = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+acoustic_guitar = {'Beginner': ['\"Wonderwall\" by Oasis', '\"What\'s Up\" by 4 Non Blondes', '\"I walk the Line\" by Johnny Cash', '\"Free Fallin\'\" by Tom Petty', '\"Used to Love Her\" by Guns \'n\' Roses'], 'Intermediate': ['\"Wanted Dead or Alive\" by Bon Jovi', '\"Layla (Unplugged)\" by Eric Clapton', '\"Tears in Heaven\" by Eric Clapton', '\"Stairway to Heaven\" by Led Zeppelin', '\"To Be With You\" by Mr. Big'], 'Advanced': ['\"Classical Gas\" by Tommy Emmanuel (Mason Williams)', '\"Angelina\" by Tommy Emmanuel', '\"Jolene\" by Dolly Parton', '\"Neon\" by John Mayer', '\"Mister Sandman\" by Chet Atkins']}
 electric_guitar = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
 bass = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
 drums = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
