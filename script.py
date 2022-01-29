e91666e4fb82a5fefccca94410734e9ebf1083cc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-01-29 21:05:53 +0000

    Add empty dictionaries for each instrument

---
+++ script.py
@@ -0,0 +1,6 @@
+acoustic_guitar = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+electric_guitar = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+bass = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+drums = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+piano = {'Beginner': [], 'Intermediate': [], 'Advanced': []}
+
